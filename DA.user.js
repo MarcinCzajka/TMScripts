37e51426eaed8c00409334a79240e0934a700c55
Marcin Czajka <czajka.marcin@protonmail.com>
2019-07-31 10:23:46 +0200

    .

--- DA.user.js
+++ DA.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Presety - Dane Administracyjne
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  try to take over the world!
 // @author       MAC
 // @match        */api/vehicle/admin/save/*
