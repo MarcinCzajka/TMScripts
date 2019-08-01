ed7a5c1d46a0b150e23c5be4b7e7d394b13c5729
Marcin Czajka <czajka.marcin@protonmail.com>
2019-08-01 08:11:28 +0200

    .

--- PM.user.js
+++ PM.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wypełnianie protokołu montażowego
 // @namespace    http://tampermonkey.net/
-// @version      2.4.1
+// @version      2.4.2
 // @description  try to take over the world!
 // @author       MAC
 // @match        http://*/api/installation*
