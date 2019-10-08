df0fe58685a91360a63d7c2e225024ef12bef81c
Marcin Czajka <czajka.marcin@protonmail.com>
2019-10-08 10:39:26 +0200

    .

--- Kalibracja.user.js
+++ Kalibracja.user.js
@@ -75,7 +75,7 @@
 		document.getElementById("newTextbox2").addEventListener('input', function() {makePoints(cm2, "newTextbox2")});
 		
 		//Guzik do zamiany wielkości zbiorników
-		const tankSwapBtn = `<input id="tankSwap" type="button" value="Zamień zbiorniki" style="padding:5px;height:25px;float:right"></input>`;
+		const tankSwapBtn = `<input id="tankSwap" type="button" value="Zamień zbiorniki" style="padding:5px;height:25px;float:right;margin:5px"></input>`;
 		calibrationToolkit1.insertAdjacentHTML('beforeend', tankSwapBtn);
 		
 		document.getElementById("tankSwap").addEventListener('click', function() {
