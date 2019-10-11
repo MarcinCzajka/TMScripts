737db396c10c2c4a4780f482ef7fc7736287858c
Marcin Czajka <czajka.marcin@protonmail.com>
2019-10-11 11:31:14 +0200

    .

---
+++ thuliumClose.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Close Thulium Popup
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *thulium.com/panel/softphone*
+// @grant        none
+// @include      *thulium.com/panel/softphone*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.close();
+    // Your code here...
+})();
