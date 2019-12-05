7105f5bbc417f5c2f7c3bb01ffb6aef4e1b398d9
Marcin Czajka <czajka.marcin@protonmail.com>
2019-12-05 11:06:26 +0100

    Fixed script

--- clearDatesInReport.js
+++ clearDatesInReport.js
@@ -6,22 +6,23 @@
 // @author       MAC
 // @downloadURL https://github.com/MarcinCzajka/TMScripts/raw/master/clearDatesInReport.user.js
 // @updateURL   https://github.com/MarcinCzajka/TMScripts/raw/master/clearDatesInReport.user.js
-// @match        */api/raports/*
+// @match        */api/raports/refueling*
 // @grant        none
-// @include      */api/raports/*
+// @include      */api/raports/refueling*
 // ==/UserScript==
 
 (function () {
     'use strict';
 
     $(document).ready(() => {
-        $(`<input type="button" id="clearFiltersBtn">Brak powiązania z GPS</input>`).insertBefore($('#clearFiltersBtn'));
+        const clearFiltersBtn = `<input type="button" id="clearFiltersBtn" value="Brak powiązania z GPS" style="width:135px;padding:0;margin:0 10px 0 0;background:#99a4ae"></input>`;
+        $(clearFiltersBtn).insertBefore($('#reset_filter'));
         $('#clearFiltersBtn').on('click', clearFilters);
     });
 
     function clearFilters() {
         for (let i = 1; i <= 4; i++) {
-            $('#table_calendar_input' + i).val('');
+            $('#table_calendar_input' + (i !== 1 ? i : '')).val('');
             $('select[name="show_invoice"]').select2('val', 2);
             $('#submit_filter').trigger('click');
         }
