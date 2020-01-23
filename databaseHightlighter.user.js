b3c904f5d840ed1ae73ef4f9bec22d89be68c8fe
Marcin Czajka <czajka.marcin@protonmail.com>
2020-01-23 16:17:01 +0100

    I committed to the wrong files ;f

--- databaseHightlighter.user.js
+++ databaseHightlighter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GPS Data Hightlighter
 // @namespace    https://github.com/MarcinCzajka
-// @version      0.1
+// @version      0.3
 // @description  Zaznacz kolorami komórkę w tabeli według określonych kryteriów
 // @author       MAC
 // @downloadURL  https://github.com/MarcinCzajka/TMScripts/raw/master/databaseHightlighter.user.js
@@ -11,21 +11,41 @@
 // @include *.pl/record/*
 // ==/UserScript==
 
+const coloredElements = [];
+
+const timeToWait = 3000;
+
 (function() {
     'use strict';
 
-    const headers = [];
+	const headers = [];
 
     setTimeout(() => {
+        for(let button of document.getElementsByClassName('btn')) {
+            if(button.innerText === "Filtruj" || button.innerText === "Resetuj") {
+                button.addEventListener('click', () => {setTimeout(() => {checkData()}, timeToWait)});
+            }
+		}
+
+		document.getElementById('per-page').addEventListener('change', () => {setTimeout(() => {checkData()}, timeToWait)});
+
         for(let item of document.getElementsByTagName('th')) {
             headers.push(item.children[0].innerText)
         }
 
-        loopThroughColumn("Szerokość", pozycja);
-        loopThroughColumn("Satelity", satelity);
-        loopThroughColumn("Nap. aku.", napAku);
+        checkData();
 
     }, 5000);
+
+    function checkData() {
+		clearElements();
+
+		loopThroughColumn("Szerokość", pozycja);
+		loopThroughColumn("Długość", pozycja);
+        loopThroughColumn("Satelity", satelity);
+        loopThroughColumn("Stacyjka", stacyjka);
+		loopThroughColumn("Nap. aku.", napAku);
+    }
 
     function loopThroughColumn(columnName, callback) {
         const index = headers.indexOf(columnName);
@@ -47,10 +67,36 @@ function satelity(el) {
     if(+el.innerText < 4) markError(el);
 }
 
+function stacyjka(el) {
+    if(el.innerText === 'Wył.') {
+        const voltage = +el.nextElementSibling.innerText;
+        if(voltage > 25) {
+            markAlert(el);
+            markAlert(el.nextElementSibling);
+        } else if(voltage > 13 && voltage < 20) {
+            markAlert(el);
+            markAlert(el.nextElementSibling);
+        }
+    }
+}
+
 function napAku(el) {
-    if(+el.innerText < 10) markError(el);
+    if(+el.innerText < 9) markError(el);
 }
 
 function markError(el) {
-    el.style.backgroundColor = '#ff697d';
+	el.style.backgroundColor = '#ff697d';
+	coloredElements.push(el);
+}
+
+function markAlert(el) {
+	el.style.backgroundColor = '#f2c329';
+	coloredElements.push(el);
+}
+
+function clearElements() {
+	coloredElements.forEach(function(el, index, object) {
+		el.style = '';
+		object.splice(index, 1);
+	});
 }
