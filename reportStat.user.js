b6f212dcc6a7a64272b692ae6e2360d8faad69d0
Marcin Czajka <czajka.marcin@protonmail.com>
2020-09-23 13:14:06 +0200

    Add toggle option

--- reportStat.user.js
+++ reportStat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ukryj Raporty nie zawierające danych
 // @namespace    https://github.com/MarcinCzajka
-// @version      0.0.1
+// @version      0.1.0
 // @description  Ukryj Raporty nie zawierające danych
 // @author       MAC
 // @downloadURL  https://github.com/MarcinCzajka/TMScripts/raw/master/reportStat.user.js
@@ -18,8 +18,18 @@
     stylesheet.type = "text/css";
 
     stylesheet.textContent = `
-        .noData {
-            display: none;
+        .hide {
+            display: none !important;
+        }
+        .btn {
+            padding: 0 10px !important;
+            height: 27px !important;
+        }
+        .basicColor {
+            background: #28bea9 !important;
+        }
+        .altColor {
+            background: #99a4ae !important;
         }
     `;
 
@@ -32,10 +42,37 @@
 
     for(let i = 1; i < tableHead.length - 1; i++) {
         if((fuellosses[i].innerText === '' || fuellosses[i].innerText.includes('(0)')) && (refuelings[i].innerText === '' || refuelings[i].innerText.includes('(0)'))) {
-            for(const elem of table.children) elem.children[i].classList.add('noData');
+            for(const elem of table.children) elem.children[i].classList.add('noData', 'hide');
         }
     }
 
+    const newTd = document.createElement('td');
+    const btn = document.createElement('input');
+    btn.type = 'button';
+    btn.value = 'Pokaż wszystkie firmy';
+    btn.classList.add('btn', 'altColor');
+    btn.dataset.active = 'true';
 
+    document.querySelector('#bottom_header tr').append(newTd);
+    newTd.append(btn);
+
+    btn.addEventListener('click', onClick);
+
+    function onClick() {
+        const elements = document.getElementsByClassName('noData');
+        for(const elem of elements) elem.classList.toggle('hide');
+
+        if(btn.dataset.active === 'true') {
+            btn.classList.add('basicColor');
+            btn.classList.remove('altColor');
+            btn.value = 'Ukryj wszystkie firmy'
+            btn.dataset.active = 'false';
+        } else {
+            btn.classList.add('altColor');
+            btn.classList.remove('basicColor');
+            btn.value = 'Pokaż wszystkie firmy'
+            btn.dataset.active = 'true';
+        }
+    }
 
 })();
