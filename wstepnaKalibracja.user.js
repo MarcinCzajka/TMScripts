fbecef24ace65d56f044b8657294c749c04c4039
Marcin Czajka <czajka.marcin@protonmail.com>
2020-10-29 13:09:36 +0100

    Removed unused icon

--- wstepnaKalibracja.user.js
+++ wstepnaKalibracja.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wstępna kalibracja pojazdu
 // @namespace    https://github.com/MarcinCzajka
-// @version      2.28.15
+// @version      2.28.16
 // @description  Wstępne założenie kartoteki pojazdu
 // @author       MAC
 // @downloadURL  https://github.com/MarcinCzajka/TMScripts/raw/master/wstepnaKalibracja.user.js
@@ -38,10 +38,6 @@
 	</svg>`;
   	const errorSvg = `<svg style="color: red; float:left" width="2.2em" height="2.2em" viewBox="0 0 16 16" fill="currentColor">
 		<path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-	</svg>`
-	const warningSvg = `<svg style="color: #dea524" width="${svgSize}" height="${svgSize}" viewBox="0 0 17 16" fill="currentColor">
-		<path fill-rule="evenodd" d="M7.938 2.016a.146.146 0 0 0-.054.057L1.027 13.74a.176.176 0 0 0-.002.183c.016.03.037.05.054.06.015.01.034.017.066.017h13.713a.12.12 0 0 0 .066-.017.163.163 0 0 0 .055-.06.176.176 0 0 0-.003-.183L8.12 2.073a.146.146 0 0 0-.054-.057A.13.13 0 0 0 8.002 2a.13.13 0 0 0-.064.016zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"/>
-		<path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z"/>
 	</svg>`;
 
 	//Create instance of custom class Timer, declaration of which is available below. This object is used to measure Calibration time
