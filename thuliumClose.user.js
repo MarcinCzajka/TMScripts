9593448be46a27ff10a0da96ff9382bc709ccf38
Marcin Czajka <czajka.marcin@protonmail.com>
2020-11-16 08:55:02 +0100

    Add run-at argument for tampermonkey

--- thuliumClose.user.js
+++ thuliumClose.user.js
@@ -1,19 +1,16 @@
 // ==UserScript==
 // @name         Close Thulium Popup
 // @namespace    https://github.com/MarcinCzajka
-// @version      0.1
+// @version      0.3
 // @description  Close annoying Thulium pop-up about phone not being connected
 // @author       MAC
-// @downloadURL https://github.com/MarcinCzajka/TMScripts/raw/master/thuliumClose.user.js
-// @updateURL   https://github.com/MarcinCzajka/TMScripts/raw/master/thuliumClose.user.js
+// @downloadURL  https://github.com/MarcinCzajka/TMScripts/raw/master/thuliumClose.user.js
+// @updateURL    https://github.com/MarcinCzajka/TMScripts/raw/master/thuliumClose.user.js
 // @supportURL   https://github.com/MarcinCzajka/TMScripts/issues
 // @match        *thulium.com/panel/softphone*
 // @grant        none
 // @include      *thulium.com/panel/softphone*
+// @run-at document-start
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-    window.close(); //Yeah, i know
-})();
+(function() {window.close()})();
